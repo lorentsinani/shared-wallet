d0b9722b8dfcf945edb125418800a22c5247eaf3
Lorent Sinani <lorent.sinani0@gmail.com>
2024-01-22 23:14:15 +0100

    Implement logic for allowance

--- src/Allowance.sol
+++ src/Allowance.sol
@@ -1,1 +1,46 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable(msg.sender) {
+    event AllowanceChanged(
+        address indexed _forWho,
+        address indexed _byWhom,
+        uint256 _oldAmount,
+        uint256 _newAmount
+    );
+    mapping(address => uint256) public allowance;
+
+    function isOwner() internal view returns (bool) {
+        return owner() == msg.sender;
+    }
+
+    function setAllowance(address _who, uint256 _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            isOwner() || allowance[msg.sender] >= _amount,
+            "You are not allowed!"
+        );
+        _;
+    }
+
+    function reduceAllowance(address _who, uint256 _amount)
+        internal
+        ownerOrAllowed(_amount)
+    {
+        emit AllowanceChanged(
+            _who,
+            msg.sender,
+            allowance[_who],
+            allowance[_who] - _amount
+        );
+        allowance[_who] -= _amount;
+    }
+}
 
